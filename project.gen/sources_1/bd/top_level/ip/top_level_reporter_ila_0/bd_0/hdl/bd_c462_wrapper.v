5823d3de2c089cdd731fdb861f9df5b0c93f77c1
DouglasWWolf <douglaswolf@yahoo.com>
2024-04-29 19:25:36 -0700

    No commit message

---
+++ project.gen/sources_1/bd/top_level/ip/top_level_reporter_ila_0/bd_0/hdl/bd_c462_wrapper.v
@@ -0,0 +1,57 @@
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Command: generate_target bd_c462_wrapper.bd
+//Design : bd_c462_wrapper
+//Purpose: IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module bd_c462_wrapper
+   (SLOT_0_AXIS_tdata,
+    SLOT_0_AXIS_tkeep,
+    SLOT_0_AXIS_tlast,
+    SLOT_0_AXIS_tready,
+    SLOT_0_AXIS_tvalid,
+    clk,
+    probe0,
+    probe1,
+    probe2,
+    probe3,
+    resetn);
+  input [511:0]SLOT_0_AXIS_tdata;
+  input [63:0]SLOT_0_AXIS_tkeep;
+  input SLOT_0_AXIS_tlast;
+  input SLOT_0_AXIS_tready;
+  input SLOT_0_AXIS_tvalid;
+  input clk;
+  input [0:0]probe0;
+  input [8:0]probe1;
+  input [31:0]probe2;
+  input [511:0]probe3;
+  input resetn;
+
+  wire [511:0]SLOT_0_AXIS_tdata;
+  wire [63:0]SLOT_0_AXIS_tkeep;
+  wire SLOT_0_AXIS_tlast;
+  wire SLOT_0_AXIS_tready;
+  wire SLOT_0_AXIS_tvalid;
+  wire clk;
+  wire [0:0]probe0;
+  wire [8:0]probe1;
+  wire [31:0]probe2;
+  wire [511:0]probe3;
+  wire resetn;
+
+  bd_c462 bd_c462_i
+       (.SLOT_0_AXIS_tdata(SLOT_0_AXIS_tdata),
+        .SLOT_0_AXIS_tkeep(SLOT_0_AXIS_tkeep),
+        .SLOT_0_AXIS_tlast(SLOT_0_AXIS_tlast),
+        .SLOT_0_AXIS_tready(SLOT_0_AXIS_tready),
+        .SLOT_0_AXIS_tvalid(SLOT_0_AXIS_tvalid),
+        .clk(clk),
+        .probe0(probe0),
+        .probe1(probe1),
+        .probe2(probe2),
+        .probe3(probe3),
+        .resetn(resetn));
+endmodule
--- src/revision_history.vh
+++ src/revision_history.vh
@@ -4,6 +4,9 @@
 // -----------------------------------------------------------------------------------------------
 // 27-Apr-2024  1.0.0  DWW  Initial creation
 //================================================================================================
+localparam RTL_TYPE      = 12266;
+localparam RTL_SUBTYPE   = 0;
+
 localparam VERSION_MAJOR = 1;
 localparam VERSION_MINOR = 0;
 localparam VERSION_BUILD = 0;
